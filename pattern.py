1272801120933db387bf71e306267d9442ebd8bc
frommindtopage <93480981+frommindtopage@users.noreply.github.com>
2021-10-31 20:36:17 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -10,7 +10,29 @@ a2.speed(100)
 
 a1.color("red");
 a2.color("blue");
+    e=1;d=-1
+    a1.forward(e*x)
+    a1=t.Turtle()
+a1.speed(100)
+    a1.left(e*x)
+    a1.forward(d*(x+1))
+    a1.left(d*(x+1))
+    e=-1;d=1
+    a2.forward(e*x)
+    a2.left(e*x)
+    a2.forward(d*(x+1))
+    a1.left(e*x)
+    a1.forward(d*(x+1))
+    a1.left(d*(x+1))
+    e=-1;d=1
+    a2.forward(e*x)
+    a2.left(e*x)
+    a2.forward(d*(x+1))
 
+for x in range(100):
+    e=1;d=-1
+    a1.forward(e*x)
+    a1.left(e*x)
 for x in range(100):
     e=1;d=-1
     a1.forward(e*x)
